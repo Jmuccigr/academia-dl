6377c8a438dbe269b6d08461a20af4feb3a7740c
John Muccigrosso <john@muccigrosso.org>
2020-08-28 16:26:15 +0200

    Use downloads folder; clearer errors

--- academia-dl.rb
+++ academia-dl.rb
@@ -5,30 +5,46 @@ require 'uri'
 require 'open-uri'
 require 'open_uri_redirections'
 require 'addressable/uri'
+require 'etc'
+
+uname = Etc.getlogin
 
 REFERER = 'http://scholar.google.com'
 PREFIX = 'https://www.academia.edu/download'
 OPEN_URI_OPTIONS = {"Referer" => REFERER, :allow_redirections => :all}
 
+download_dir = "/Users/#{uname}/Downloads"
+
 ARGV.each do |academia_url|
   doc = nil
+  tries = 0
   begin
     uri = Addressable::URI.parse(academia_url).normalize.to_s
     doc = Nokogiri::HTML(URI.open(uri))
   rescue OpenURI::HTTPError => e
-    $stderr.puts e.inspect
-    sleep(5)
-    retry
+    tries += 1
+    if tries < 5
+      sleep(5)
+      retry
+    else
+      $stderr.puts 'URL problem:\n' + e.inspect
+      exit
+    end
   end
-  download_url = doc.css('a.js-swp-download-button').first['href']
-  download_id = download_url.split('/')[-2]
-  filename = "#{URI(uri).path.split('/').last[0..250]}.pdf"
-  url = "#{PREFIX}/#{download_id}/#{filename}"
-  $stderr.puts "Resolved download URL: #{url}"
-  if File.exist?(filename)
-    $stderr.puts "#{filename} already exists, skipping"
+  begin
+	download_url = doc.css('a.js-swp-download-button').first['href']
+	download_id = download_url.split('/')[-2]
+	filename = "#{URI(uri).path.split('/').last[0..250]}.pdf"
+	url = "#{PREFIX}/#{download_id}/#{filename}"
+  rescue
+    $stderr.puts "Problem with the download. Here's what I was trying to get:\n<#{url}>"
+    exit
+  end
+#   $stderr.puts "Resolved download URL: #{url}"
+  if File.exist?("#{download_dir}/#{filename}")
+    $stderr.puts "File already exists" #", skipping:\n#{filename}"
   else
-    IO.copy_stream(open(url, OPEN_URI_OPTIONS), filename)
-    $stderr.puts "Downloaded #{filename}"
+    IO.copy_stream(open(url, OPEN_URI_OPTIONS), "#{download_dir}/#{filename}")
+#     $stderr.puts "Downloaded #{filename}"
   end
 end
